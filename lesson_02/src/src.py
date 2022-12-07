82726ae33a4a5e1f47cca57b24c98584b46c7914
Dmytro Kovryzhenko <onemoremagistr@gmail.com>
2022-12-07 17:07:11 +0200

    -init_bonuses

--- lesson_02/src/src.py
+++ lesson_02/src/src.py
@@ -15,6 +15,16 @@ def create_enemy():
 	# Let's return dictionary, because we can see what kind of object there by keys
 	return {"enemySurface": enemy, "enemyRectangle": enemyRectangle, "enemyDeltaPos": enemyDeltaPos}
 
+def create_bonus():
+	
+	bonusSize = (20, 20)	
+	bonus = pygame.Surface(bonusSize)
+	bonus.fill(GREEN)
+	bonusRectangle = pygame.Rect(random.randint(0, width), 0, *bonusSize)
+	bonusDeltaPos = {"x": 0, "y":  random.randint(4, 7)}
+
+	# Let's return dictionary, because we can see what kind of object there by keys
+	return {"bonusSurface": bonus, "bonusRectangle": bonusRectangle, "bonusDeltaPos": bonusDeltaPos}
 
 if __name__ == "__main__":
 
@@ -41,9 +51,6 @@ if __name__ == "__main__":
 
 	RANDOM_COLORS = [WHITE, RED, ORANGE, YELLOW, GREEN, BLUE, SLATEBLUE, ROSYBROWN]
 	
-	DELTAS_ENEMY = [0.2, 0.3, 0.4, 0.5]
- 
- 
 	mainWindow = pygame.display.set_mode(screen)
 	mainWindow.fill((155, 155, 155))
 	
@@ -52,49 +59,52 @@ if __name__ == "__main__":
 	ball.fill(WHITE)
 	ballRectangle = ball.get_rect()
 	ballRectangle = ballRectangle.move((0, 0))
-	# ballSpeed = [1, 1]
 	ballDeltaPos = {"x": 5, "y": 5}
- 
-	# enemySize = (20, 20)	
-	# enemy = pygame.Surface(enemySize)
-	# enemy.fill(RED)
-	# enemyRectangle = pygame.Rect(width, 100, *enemySize)
-	# enemyDeltaPos = {"x":1, "y":1}
 
 	CREATE_ENEMY = pygame.USEREVENT + 1
 	pygame.time.set_timer(CREATE_ENEMY, 1500)
-
 	enemies = []
+
+	CREATE_BONUS = pygame.USEREVENT + 1
+	pygame.time.set_timer(CREATE_BONUS, 2500)
+	bonuses = []
 	
+	# Game cycle
 	running = True
 	while running:
 		
+		# Frame per second settings
 		FPS.tick(60)
 		
+		# Event handling
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				running = False
 			if event.type == CREATE_ENEMY:
 				enemies.append(create_enemy())		
-		
-		pressedKeys = pygame.key.get_pressed()
+			if event.type == CREATE_BONUS:
+				bonuses.append(create_bonus())		
   
 		# Redraw main window to black every iteration
 		mainWindow.fill(BLACK)
 		# Redraw ball on top of the main window
 		mainWindow.blit(ball, ballRectangle)
-  		# Redraw enemy on top of the main window
+  		# Redraw enemy on top of the previous
 		for enemy in enemies:
 			mainWindow.blit(enemy["enemySurface"], enemy["enemyRectangle"])
+  		# Redraw bouneses on top of the previous
+		for bonus in bonuses:
+			mainWindow.blit(bonus["bonusSurface"], bonus["bonusRectangle"])
 
 		# User controls
-		if pressedKeys[K_DOWN]:
+		pressedKeys = pygame.key.get_pressed()
+		if pressedKeys[K_DOWN] and ballRectangle.bottom < height:
 			ballRectangle = ballRectangle.move([0,ballDeltaPos["y"]])
-		if pressedKeys[K_UP]:
+		if pressedKeys[K_UP] and ballRectangle.top > 0:
 			ballRectangle = ballRectangle.move([0,-ballDeltaPos["y"]])
-		if pressedKeys[K_LEFT]:
+		if pressedKeys[K_LEFT] and ballRectangle.left > 0:
 			ballRectangle = ballRectangle.move([-ballDeltaPos["x"],0])
-		if pressedKeys[K_RIGHT]:
+		if pressedKeys[K_RIGHT] and ballRectangle.right < width:
 			ballRectangle = ballRectangle.move([ballDeltaPos["x"],0])
 
 		# Manipulate enemies
@@ -109,6 +119,19 @@ if __name__ == "__main__":
 
 			if ballRectangle.colliderect(enemy["enemyRectangle"]):
 				enemies.pop(enemies.index(enemy))
+
+		# Manipulate bonuses
+		for bonus in bonuses:
+			# Move enemies
+			bonus["bonusRectangle"] = bonus["bonusRectangle"].move([-bonus["bonusDeltaPos"]["x"], bonus["bonusDeltaPos"]["y"]])
+			# Delete enemies
+			# NOTE: we assume that bonus should be deleted when it's TOP side
+			# 		go outside the game field, so it's FULL BODY go outside
+			if bonus["bonusRectangle"].top > height:
+				bonuses.pop(bonuses.index(bonus))
+
+			if ballRectangle.colliderect(bonus["bonusRectangle"]):
+				bonuses.pop(bonuses.index(bonus))
 
 		# Show every drawing to screen
 		pygame.display.flip()
